69047b158c4a60a30d4cd4f919569d6aea77f58f
Fahad <fahadmahmood1200@gmail.com>
2022-01-12 10:31:23 -0500

    Added orderPage

--- App.js
+++ App.js
@@ -9,7 +9,12 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName={"Home"}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Home"}
+      >
         <Stack.Screen name="Tabs" component={Tabs} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
@@ -17,5 +22,5 @@ function App() {
     </NavigationContainer>
   );
 }
- 
+
 export default App;
